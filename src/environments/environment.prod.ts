620082914316728a92a60494c7ace77a4fc37897
Mario Elsnig <46848943+MarioMatschgi@users.noreply.github.com>
2021-07-03 10:32:18 +0200

    Setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAkDxUxw_sTbewd45_sQIW91BEMHdK5fqU',
+    authDomain: 'whitenotes-d32a9.firebaseapp.com',
+    projectId: 'whitenotes-d32a9',
+    storageBucket: 'whitenotes-d32a9.appspot.com',
+    messagingSenderId: '574664165888',
+    appId: '1:574664165888:web:bd7b8ee6bf25780c0e26e5',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAkDxUxw_sTbewd45_sQIW91BEMHdK5fqU',
+    authDomain: 'whitenotes-d32a9.firebaseapp.com',
+    projectId: 'whitenotes-d32a9',
+    storageBucket: 'whitenotes-d32a9.appspot.com',
+    messagingSenderId: '574664165888',
+    appId: '1:574664165888:web:bd7b8ee6bf25780c0e26e5',
+  },
 };
 
 /*
